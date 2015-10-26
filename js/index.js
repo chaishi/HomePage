87c85a450174caa72818deba0dfdc59c648a50c1
luoxue <974383157@qq.com>
2015-10-26 16:46:07 +0800

    加入我们（招贤纳士）页面完成

--- js/index.js
+++ js/index.js
@@ -7,16 +7,22 @@
 $(function(){
 	var pathname = location.pathname;
 	switch(pathname) {
-		case '/HomePage/index.html': {page_home.getHomeHtml();getTopbarHtml(0);}break;
-		case '/HomePage/joinUs.html': {getTopbarHtml(3);}break;
+		case '/HomePage/index.html': {
+			page_home.getHomeHtml();
+			getTopbarHtml(0);
+		}break;
+		case '/HomePage/joinUs.html': {
+			getTopbarHtml(3);
+			page_hiring.init();
+		}break;
 		default:{}break;
 	}
 	getFooterHtml();
 });
 
-/*=====================================================
+/*=================================================================================================
  * @description 公共js 
- ======================================================*/
+ ==================================================================================================*/
 
 (function(window, undefined){
 	
@@ -25,6 +31,7 @@ $(function(){
 		$('.topbar-right').on('click', 'li', switchPage);
 	}
 	
+	//页面切换
 	function switchPage() {
 		var pageTitle = $(this).html();
 		switch(pageTitle) {
@@ -36,12 +43,18 @@ $(function(){
 		}
 	}
 	
-	function setTopbarActive(index) {
-		$('.topbar-right li').each(function(i) {
+	/*
+	 * @description 将列表中的某一项设为激活状态
+	 * @param {Number} index 将下标为index的那一项设为激活状态
+	 * @param {String} select 列表选择器
+	 * @param {String} classi 用来激活列表的class类
+	 */
+	window.setActive = function(index, select, classi) {
+		$(select).each(function(i) {
 			if(i === index) {
-				$(this).addClass('topbar-bg');
+				$(this).addClass(classi);
 			}else {
-				$(this).removeClass('topbar-bg');
+				$(this).removeClass(classi);
 			}
 		})
 	}
@@ -57,7 +70,7 @@ $(function(){
 			success: function(data) {
 				$('#topbar').html(data);
 				addClickToTop();
-				setTopbarActive(index);
+				setActive(index, '.topbar-right li', 'topbar-bg');
 			}
 		});
 	}
@@ -75,27 +88,22 @@ $(function(){
 	
 })(window);
 
-/* =====================================================
+/* ===========================================================================================
  * @description 首页相关js
  * @author luoxue
  * @time 20151022
- =======================================================*/
+ =============================================================================================*/
 
 var page_home = {};
 
 (function(page_home, undefined){
-	
-	function init() {
-		getTopbarHtml();
-		getFooterHtml();
-	}
 	
 	//事件绑定
 	function bindEvent() {
 		$('.content-circles').on('click', '.one-circle', function() {
 			var index = $(this).index();
 			getSlideScreen( index );
-			setCircleActive( index );
+			setActive( index, '.one-circle', 'circle-bg' );
 		});
 	}
 	
@@ -107,21 +115,7 @@ var page_home = {};
 		var margin = 100 * index;
 		$('.content-screens').animate({marginLeft: '-' + margin + '%'});
 	}
-	
-	/*
-	 * @description 激活小圆点
-	 * @param {Number} index 小圆点下标。用户点击某个小圆点时，将某个小圆点设置为激活状态，其它小圆点为非激活状态
-	 */
-	function setCircleActive(index) {
-		$('.one-circle').each(function(i){
-			if(i === index) {
-				$(this).addClass('circle-bg');
-			}else {
-				$(this).removeClass('circle-bg');
-			}
-		});
-	}
-	
+		
 	//加载首页content html
 	page_home.getHomeHtml = function(){
 		$.ajax({
@@ -137,16 +131,30 @@ var page_home = {};
 })(page_home)
 
 
-/* =====================================================
+/* ==================================================================================================
  * @description 招贤纳士（加入我们） 相关js
  * @author luoxue
  * @time 20151022
- =======================================================*/
+ ====================================================================================================*/
 
 var page_hiring = {};
 
-(function(page,undefined){
+(function(page, undefined){
 	
+	page.init = function() {
+		bindEvent();
+	};
+	
+	function bindEvent() {
+		$('.join-city').on('click', '.one-city', cityChange);
+	}
+	
+	//工作城市切换
+	function cityChange() {
+		var index = $(this).index();
+		setActive(index / 2, '.one-city', 'city-active');
+		setActive(index / 2, '.orange-rect', 'visi-show');
+	}
 	
 	
 })(page_hiring);
